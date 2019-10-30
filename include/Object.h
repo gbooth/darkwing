6d9326b76a1278f0536ce933eb16846436328c66
hunb2720 <ben.hunt@uleth.ca>
2019-10-30 16:52:46 -0600

    Currently nothing works.

--- include/Object.h
+++ include/Object.h
@@ -1,15 +1,50 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include<map>
+#include<string>
 
 class Object
 {
   public:
-    Object(Identifier i) :id{i};
+     /**
+    *an enum holding all of our identifiers
+    */
+    enum Identifier{chest, sword, spear, axe, Fred};
+
+    /**
+    *constructor
+    */
+    Object(Identifier a = sword) :id{a}, description{ObjectMap[a]} {};
+
+    /**
+    *destructor
+    */
     virtual ~Object();
 
+
+    /**
+    *a map that links an identifier to a description
+    */
+    std::map<Identifier, std::string> ObjectMap;
+
+    /**
+    *sets will set ObjectMap to map to a description
+    */
+    void setMap();
+
+    /**
+    *get Object Identifier
+    */
+    Identifier getID();
+
+    /**
+    *get Object Description
+    */
+    std::string getDesc();
+
   protected:
-    string description;
+    std::string description;
     Identifier id;
 
   private:
