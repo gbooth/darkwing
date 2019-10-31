db462c9d5f9b77fd713aeaad9eaa52fe401d5780
hunb2720 <ben.hunt@uleth.ca>
2019-10-31 10:04:50 -0600

    Object class

--- include/Object.h
+++ include/Object.h
@@ -4,50 +4,48 @@
 #include<map>
 #include<string>
 
-class Object
-{
-  public:
-     /**
-    *an enum holding all of our identifiers
-    */
-    enum Identifier{chest, sword, spear, axe, Fred};
+class Object {
+ public:
+  /**
+  *an enum holding all of our identifiers
+  */
+  enum Identifier {goldenChest, Chest, goldenSword, sword, goldenDagger,
+                   dagger, goblin1, goblin2, goblin3, goblin4, duckDragon,
+                   jzargo, serana, lydia, greyBeard, jackSparrow, billTurner,
+                   dinkleBerg, kingOrlando, phyllis, healthPotion1,
+                   healthPotion2, blueLock, orangeLock, blueKey, orangeKey, lever1, lever2,
+                   lever3, lever4, lever5, lever6
+                  };
+  /**
+  *constructor
+  */
+  Object(Identifier a = sword) :id{a}, description{ObjectMap[a]} {};
 
-    /**
-    *constructor
-    */
-    Object(Identifier a = sword) :id{a}, description{ObjectMap[a]} {};
+  /**
+  *virtual destructor
+  */
+  virtual ~Object();
 
-    /**
-    *destructor
-    */
-    virtual ~Object();
+  /**
+  *a map that links an identifier to a description
+  */
+  std::map<Identifier, std::string> ObjectMap;
+
+  /**
+  * @return description
+  */
+  std::string getDesc();
+
+  /**
+  *the Object Identifier
+  */
+  const Identifier id;
 
 
-    /**
-    *a map that links an identifier to a description
-    */
-    std::map<Identifier, std::string> ObjectMap;
+ protected:
+  std::string description;
 
-    /**
-    *sets will set ObjectMap to map to a description
-    */
-    void setMap();
-
-    /**
-    *get Object Identifier
-    */
-    Identifier getID();
-
-    /**
-    *get Object Description
-    */
-    std::string getDesc();
-
-  protected:
-    std::string description;
-    Identifier id;
-
-  private:
+ private:
 };
 
 #endif // OBJECT_H
