d97106128b0c4419830520133a456d8c9dbb7026
hunb2720 <ben.hunt@uleth.ca>
2019-11-01 15:48:30 -0600

    proof of concept for object

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,5 +1,14 @@
 #include "Object.h"
+#include <map>
+#include <fstream>
+#include <iostream>
 
-int main(){
-return 0;
+std::map<Object::Identifier, std::string> Object::ObjectMap;
+
+int main() {
+  Object::setMap();
+  Object a(Object::dagger);
+  std::cout << "your id: " << a.getID() << std::endl;
+  std::cout << "your description: " << a.description << std::endl;
+  return 0;
 }
