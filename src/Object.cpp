277b074dff33bdfad533e48dc6dbce6c313354f9
hunb2720 <ben.hunt@uleth.ca>
2019-11-02 12:09:41 -0600

    Changing object to int for id and file to map ints to names

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,74 +1,26 @@
 #include "Object.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-//using namespace std;
-
+#include"Exceptions.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<typeinfo>
+#include<utility>
 
 Object::~Object() {}
 
+void Object::setPairs(){
+  ifstream
+}
+
 void Object::setMap() {
   int colon = 0;
-  std::map<std::string, Identifier> change;
-  change["goldenChest"] = goldenChest;
-  change["Chest"] = Chest;
-  change["goldenSword"] = goldenSword;
-  change["sword"] = sword;
-  change["goldenDagger"] = goldenDagger;
-  change["dagger"] = dagger;
-  change["goblin1"] = goblin1;
-  change["goblin2"] = goblin2;
-  change["goblin3"] = goblin3;
-  change["goblin4"] = goblin4;
-  change["duckDragon"] = duckDragon;
-  change["jzargo"] = jzargo;
-  change["serana"] = serana;
-  change["lydia"] = lydia;
-  change["greyBeard"] = greyBeard;
-  change["jackSparrow"] = jackSparrow;
-  change["billTurner"] = billTurner;
-  change["dinkleBerg"] = dinkleBerg;
-  change["phyllis"] = phyllis;
-  change["healthPotion1"] = healthPotion1;
-  change["healthPotion2"] = healthPotion2;
-  change["blueLock"] = blueLock;
-  change["orangeLock"] = orangeLock;
-  change["blueKey"] = blueKey;
-  change["orangeKey"] = orangeKey;
-  change["lever1"] = lever1;
-  change["lever2"] = lever2;
-  change["lever3"] = lever3;
-  change["lever4"] = lever4;
-  change["lever5"] =lever5 ;
-  change["lever6"] = lever6;
   std::string line;
-  std::string key;
-  std::string val ;
+  std::string name;
+  std::string desc;
   std::ifstream objectData;
-  objectData.open("obf.txt");
-  if (objectData.is_open()) {
-    while (getline(objectData, line)) {
-      for (uint i = 0; i < line.size(); i++) {
-        if (line[i] == ':') {
-          colon = i;
-          key = line.substr(0, colon);
-          val = line.substr(colon + 1);
-          for (auto it = change.begin(); it != change.end(); it++) {
-            if (it->first == key) {
-              ObjectMap[it->second] = val;
-            }
-          }
-          val = "";
-          key = "";
-        }
-      }
-    }
-    objectData.close();
-  } else {
-    std::cout << "no file to open"<<std::endl;
-  }
-
+  objectData.open()
+  if(objectData.is_open)
 }
 
 std::string Object::getID() {
@@ -169,7 +121,7 @@ std::string Object::getID() {
     return "lever6";
     break;
   default:
-    return "not an identifier";
+    throw not_an_identifier_error("not specified identifier");
     break;
   }
 }
