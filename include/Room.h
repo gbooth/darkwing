6f6defb0b19b091c5a196997e971722d258ea752
Gavin (Gavin) Booth <boog2720@36venom.cs.uleth.ca>
2019-11-02 09:46:32 -0600

    Room header

--- include/Room.h
+++ include/Room.h
@@ -4,15 +4,18 @@
 #include "Object.h"
 
 
-class Room : public Object
-{
-  public:
-    Room();
-    virtual ~Room();
-
-  protected:
-
-  private:
+class Room : public Object {
+ public:
+  Room(int, int);
+  virtual ~Room();
+  bool getHasEnemy();
+  enum Direction {north, south, east, west};
+ private:
+  list<Identifiers> npcInRoom;
+  list<Identifiers> objectsInRooms;
+  map<Direction, pair<bool, pair<int, int>>> adjoiningRooms;
+  bool hasEnemy;
+  list<Identifiers> changedObjects;
 };
 
 #endif // ROOM_H
