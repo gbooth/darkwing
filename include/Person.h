89a90bd95ea659d45d7fbd88b8a237ade24df4b9
hunb2720 <ben.hunt@uleth.ca>
2019-11-08 12:21:48 -0700

    Person update

--- include/Person.h
+++ include/Person.h
@@ -7,42 +7,64 @@
 #include "Object.h"
 #include <utility>
 
-/**
-*This class is the declaration for Persons that will be included in the game  in the
-*game CastleScape.
-*/
-
 class Person : public Object {
  public:
-  /**The Persons,Villagers and the Enemies will be describe when the whole rooms are defined.
-  *also the decision if the Villagers and Enemies will be identified in the clas*ses Vil*age*r and Enemy or create one specific class for Identifiers.
-  /** The constructor behavior is to create a Person ,in this case because the class is abstract, we can not intanciate a Person object.g
-   *@parameters for identification and position of the Person .
-  */
-  Person(int id, int damageVal, std::pair<int, int> pos, int hp = 10);
 
   /**
-   *  Attack is a pure virtual method to be used for the Enemies and the Hero.
-   */
-  virtual int Attack ()=0;
+  * Constructor
+  * @param id the identifer of the person
+  * @param damageVal the damage value of the person
+  * @param pos the position of the person
+  * @param hp the health of the person
+  * @throw throw an error if id doesn't match a person
+  */
+  Person(int id, int damageVal, std::pair<int, int> pos, int hp = 10);
 
   /**
   *default Destructor
   */
   virtual ~Person();
 
-  void setPosition(std::pair<int, int>);
+  /**
+  *  Attack is a pure virtual method to be used for the Enemies and the Hero.
+  */
+  virtual int Attack ()=0;
+  /**
+  *set position of the person
+  * @param pos the new position
+  */
+
+  void setPosition(std::pair<int, int> pos);
+
+  /**
+  *@return the position
+  */
   std::pair<int, int> getPos();
+
+  /**
+  *set health of character
+  */
   void setHealth(int);
+
+  /**
+  *@return health of character
+  */
   int getHealth();
+
+  /**
+  *set damage of character
+  */
   void setDamage(int);
+
+  /**
+  *@return damage of character
+  */
   int getDamage()const;
+
  protected:
   std::pair<int, int>position;
   int health;
   int damageValue;
-
- private:
 };
 
 #endif // PERSON_H
--- src/Person.cpp
+++ src/Person.cpp
@@ -5,52 +5,35 @@
 #include "Person.h"
 #include "utility"
 #include "Exceptions.h"
-#include "Object.h"
-
-/** this is an abstract class then there is no creation of objects
- */
 
 Person::Person(int id, int damageVal, std::pair<int, int>pos, int hp)
-: Object{id}, damageValue{damageVal}, position{pos}, health{hp} {}
-/**
-*destructor function.
-*/
+: Object{id}, damageValue{damageVal}, position{pos}, health{hp} {
+  if(id /1000 != 3 || id < 3000 || id > 3300)
+    throw invalid_id("ERROR: this ID does not describe a person");
+}
+
 Person::~Person() {}
 
-/**
-*the function attack is a pure virtual function
-*/
-
-/**
-*this implementation set  anytime the position for the player.
-*/
 void Person::setPosition (std::pair<int, int> pos) {
   position = pos;
 }
-/** This function gives the position for the player.
- */
 
 std::pair<int, int> Person::getPos() {
   return position;
 }
-/** This method set the health for the player anytime.
- */
 
 void Person::setHealth(int hp) {
   health=hp;
 }
-/** This method return the health of the player anytime.
- */
+
 int Person::getHealth () {
   return health;
 }
-/** This function give the damage caused when losing point in the game.
- */
+
 void Person::setDamage(int damValue) {
+  damageValue = damValue;
 }
 
-/** This method give you the damage value in real time for the player.
- */
 int Person::getDamage()const {
   return damageValue;
 }
