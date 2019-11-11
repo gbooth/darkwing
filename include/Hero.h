d6ebe1965ccc2879528b500436de4f077eb85198
Wesley (Wesley) Waldern <walw2720@36punisher.cs.uleth.ca>
2019-11-11 13:43:43 -0700

    updated hero files, not finished yet

--- include/Hero.h
+++ include/Hero.h
@@ -5,17 +5,26 @@
 #include <string>
 //#include <Room.h>
 #include <Item.h>
+#include <map>
+#include <Direction.h>
 
 class Hero : public Person
 {
   public:
-    Hero(int, int, std::pair<int,int>pos , int);
+    Hero(int, std::pair<unsigned int,unsigned int>);
     virtual ~Hero();
+    void attack(Person*);
     std:: string inspect(Object);
-    //void mv(Direction);
-  protected:
+    Item getWeapon();
+    void setWeapon(Item);
+    void mv(Direction);
+    void setPosition(std::pair<unsigned int, unsigned int>);
+    std::pair<unsigned int, unsigned int> getPos();
 
   private:
+    std::map<Item,int>  inventory;
+    Item weaponOfChoice = 4205;
+    std::pair<uint, uint>position;
 };
 
 #endif // HERO_H
--- src/Hero.cpp
+++ src/Hero.cpp
@@ -2,16 +2,52 @@
 #include <string>
 #include <utility>
 #include <Exceptions.h>
+#include <iostream>
 
-Hero::Hero(int id = 3101, int dmg =1,
-std::pair<int, int> pos = std::make_pair(0, 0), int health = 10): Person{id,dmg,pos,health}{
-  if (id/100%10 != 1)
+Hero::Hero(int id = 3101,std::pair<unsigned int, unsigned int> pos = std::make_pair(0, 0))
+  : Person{id} {
+  if (id / 100 % 10 != 1)
     throw invalid_id("ERROR: THIS ISNT DUCK NORRIS");
 }
 
+Hero::~Hero() {}
+
+void Hero::setWeapon(Item w) {
+  if (w.getID() / 100 % 10 == 2)
+    weaponOfChoice = w;
+}
+
+Item Hero::getWeapon() {
+  return weaponOfChoice;
+}
+
+std::string Hero::inspect(Object a) {
+  return a.getDesc();
+}
+
+void Hero::mv(Direction a){
+
+}
+
+void Hero::setPosition (std::pair<uint, uint> pos) {
+  if(pos.first > 4  || pos.second > 4)
+    throw invalid_pos("ERROR: position out of bounds");
+  position = pos;
+}
+
+std::pair<uint, uint> Hero::getPos() {
+  return position;
+}
 
 
-Hero::~Hero() {
-  //dtor
+void Hero::attack(Person* npc) {
+/*  Item temp(4203);
+  if (npc -> Villager) {
+    std::cout<<"You attacked a villager and they hauled you off to jail for a life time"<<endl;
+    std::cout<<"You Lose"<<endl;
+  }
+  if(npc -> Enemy){
+    enemy.sethealth(health - (dmg + ))
+  }*/
 }
 
