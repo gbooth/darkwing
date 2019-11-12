3f82152daa3aae8d8df433582a9f7abec8cba962
Jorge (Jorge) Aceytuno <acej2720@36spiderman.cs.uleth.ca>
2019-11-11 18:02:51 -0700

    added Chest.h doxyfile format

--- include/Chest.h
+++ include/Chest.h
@@ -3,13 +3,32 @@
 
 #include <RoomObject.h>
 #include <Item.h>
+/**
+ *This class is used to access the contents inside the chest.
+ */
 
 class Chest: public RoomObject {
 
  public:
+  /**
+   *Default constructor ,that identify the objects inside the chest.
+   * @param int that is the identifier of the  objects inside the chest.
+   */
   Chest(int);
+  /**
+   * Default destructor.
+   */
   virtual~Chest();
-  Item getContents();
+  /**
+   *This function is in charge to access the identifier of the object.
+   *@return the identifier of the objects inside the chest.
+   */
+   Item getContents();
+   /**
+    *This variable contains the object identifier.
+    *The identifier 4101 ,is to have an at least one initial object at the beginning, means the chest is not empty.
+    */
+
 
  private:
   Item contents = 4101;
