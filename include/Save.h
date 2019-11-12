8cdd4d7b59b2e6208dd2b7529bae11789130a046
hunb2720 <ben.hunt@uleth.ca>
2019-11-12 12:01:36 -0700

    the finished Save class

--- include/Save.h
+++ include/Save.h
@@ -1,3 +1,8 @@
+/**
+* @author Ben Hunt
+* @date Fall 2019
+*/
+
 #ifndef SAVE_H
 #define SAVE_H
 
@@ -12,18 +17,31 @@
 
 class Save {
  public:
+  /**
+  *Constructor
+  *@param h the pointer to the hero to save
+  *@param r the pointer to the world that needs to be saved
+  */
+  Save(const Hero* h, const Room** r);
 
-  Save(Hero* h, Room** r);
-
+  /**
+  *virtual destructor
+  */
   virtual ~Save();
-
+  /**
+  *the function to save the game entirely
+  * @throw file_error if save file doesn't open
+  */
   void saveGame();
+
  private:
+   /**< helper function to save Hero */
   void saveHero();
+  /**< helper function to save room */
   void saveRoom();
   std::ofstream saveFile;
   std::map<HeroAtt, std::string data> heroSaveMap;
-  std::map<int roomID, std::list<int objID>> roomObj;
+  std::map<int roomID, std::string> roomObj;
   std::string fileName;
 };
 
