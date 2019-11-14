a87122874c9da651b0cf067c6f0afd2551bb685b
hunb2720 <ben.hunt@uleth.ca>
2019-11-14 15:10:11 -0700

    changed the order of the enums

--- include/Command.h
+++ include/Command.h
@@ -1,6 +1,6 @@
 #ifndef COMMAND_H_INCLUDED
 #define COMMAND_H_INCLUDED
 
-enum Command{go, use, inspect, inv, talk, attack, flip, open, help, equip, save};
+enum Command{use, inspect, inv, talk, attack, go,flip, open, help, equip, save};
 
 #endif // COMMAND_H_INCLUDED
