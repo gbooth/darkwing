7f7d17acadbdefbf8987dbaf668990fdbe2924bf
hunb2720 <ben.hunt@uleth.ca>
2019-11-17 12:57:52 -0700

    all test pass with good coverage

--- test/TestLock.cpp
+++ test/TestLock.cpp
@@ -8,11 +8,12 @@
 #include"Item.h"
 
 TEST(TestLock, TestConstructor) {
-
-  EXPECT_THROW(Lock a (3001), invalid_id);
+  EXPECT_THROW(Lock a(3001), invalid_id);
 }
 
 TEST(TestLock, TestUnlock) {
   Lock a(2202);
+  Lock b(2203);
+  b.unlock(4303);
   a.unlock(4301);
 }
