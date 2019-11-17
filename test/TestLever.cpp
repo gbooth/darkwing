b8a8c88d6149056a6ff8f1779a20fd61c8d58d70
hunb2720 <ben.hunt@uleth.ca>
2019-11-17 13:25:55 -0700

    tests pass with good coverage

--- test/TestLever.cpp
+++ test/TestLever.cpp
@@ -1,10 +1,24 @@
 #include "gtest/gtest.h"
 #include "Lever.h"
 #include "Exceptions.h"
+#include <utility>
 
 TEST(TestLever, ctorTest) {
   Lever l(2303);
   EXPECT_EQ(false, l.getState());
   EXPECT_THROW(Lever(1000), invalid_id);
   EXPECT_NO_THROW(Lever a(2301));
+}
+
+TEST(TestLever, setGetTest) {
+  Lever a(2303);
+  Lever* aptr = &a;
+  bool b = true;
+  std::pair<Lever*, bool> l;
+  l = std::make_pair(aptr, b);
+  a.setDepLever(l);
+  std::vector<std::pair<Lever*, bool>> vec;
+  vec.push_back(l);
+  EXPECT_TRUE(a.getDepLever() == vec);
+  aptr = nullptr;
 }
