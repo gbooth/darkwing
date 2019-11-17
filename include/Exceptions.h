2f348a2e78881ccad2006b814bcf169e15422965
Gavin Booth <gavinbooth@Gavins-MacBook-Pro.local>
2019-11-17 01:49:35 -0700

    Added save_error exception

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -60,4 +60,17 @@ class combat_error: public std::runtime_error {
   }
 };
 
+/**
+* exception for save error
+*/
+class save_error: public std::runtime_error {
+ public:
+  /**
+  * @param errMessage An error message.
+  */
+  explicit save_error(const char* errMessage) :
+    std::runtime_error(errMessage) {
+  }
+};
+
 #endif  // EXCEPTIONS_H_INCLUDED
