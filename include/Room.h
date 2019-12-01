2235e2c90ddb97e8df0b46469d7048ced357c2b2
hunb2720 <ben.hunt@uleth.ca>
2019-11-30 18:43:34 -0700

    edited tags

--- include/Room.h
+++ include/Room.h
@@ -27,9 +27,10 @@ class Person;
 class Room : public Object {
  public:
   /**
-  *Default constructor
+  *constructor
+  *@param a the id of the room
   */
-  Room(int);
+  Room(int a);
 
   /**
   *Destructor
@@ -43,34 +44,38 @@ class Room : public Object {
   bool checkForEnemy();
 
   /**
-   *This function set the Enemy to false.
+   *can change hasEnemy value
    */
   void setHasEnemy();
 
   /**
    *This function gives if NPC in the room or not.
+   *@param a the id to check for in the room
    *@throw invalid_id error"id not for a person"
    *@return true
    */
-  bool checkForNPC(int);
+  bool checkForNPC(int a);
 
   /**
    *Function that give ,if there is an object in the room.
+   *@param a the id of the object to check for
    *@throw invalid_id Error:"id not for room object"
    *@return true.
    */
-  bool checkForObj(int);
+  bool checkForObj(int a);
 
   /**
    *Checks the directions for adjacent room to move.
+   *@param d the direction to check
    */
-  std::pair<int, int> getDirection(Direction);
+  std::pair<int, int> getDirection(Direction d);
 
   /**
    *this function identify the object in the room.
+   *@param a the id of the object changed
    *@throw invalid_id Error:"obj not in this room"
    */
-  void objChanged(int);
+  void objChanged(int a);
 
   /**
    *This function will give you message related with the rooms.
@@ -86,27 +91,32 @@ class Room : public Object {
 
   /**
    *Set the coordinates of the rooms.
+   *@param r the world containing the doors
    *@throw file_error "door depend file is missing"
    */
-  void setDoor(Room**);
+  void setDoor(Room** r);
 
   /**
    * Gives the direction of the door and the state.
+   * @param a the direction of the door
+   * @param b the state of the door
    * @throw file_error"door depend file is missing"
    */
-  void upDoorMood(Direction, DoorMood);
+  void upDoorMood(Direction a, DoorMood b);
 
   /**
    *Detects if there is an NPC in the room.
-   *@param int that is the id to build
+   *@param a the id of the NPC to return
    *@return constant person pointer
    */
-  Person* const getNPC(int);
+  Person* const getNPC(int a);
 
   /**
    *This function gives he identifier of the object in the room.
+   *@param a the id of the object to get in the room
+   *@return pointer to the RoomObject you are searching for
    */
-  RoomObject* const getObj(int);
+  RoomObject* const getObj(int a);
 
  private:
   std::map<int, Person*> npcInRoom;
